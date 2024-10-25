092602d1d5f6a2ba1e18802f4948d64bff8848ee
Jade <asleepydoggy@gmail.com>
2024-10-26 03:54:05 +1100

    initial commit

---
+++ TackleBox/Mod.cs
@@ -0,0 +1,13 @@
+﻿using GDWeave;
+
+namespace TackleBox;
+
+public class Mod : IMod {
+    public Mod(IModInterface modInterface) {
+        modInterface.Logger.Information("Hello, world!");
+    }
+
+    public void Dispose() {
+        // Cleanup anything you do here
+    }
+}
