2426f7d088a0c0fb1ca258842684eb2efb22d4e0
Jade <asleepydoggy@gmail.com>
2024-10-31 08:18:16 +1100

    feat: add text field for config options

--- project/mods/TackleBox/Scenes/ModMenu/mod_config.gd
+++ project/mods/TackleBox/Scenes/ModMenu/mod_config.gd
@@ -15,33 +15,46 @@ func initialise(mod_id: String) -> void:
 	var mod_name: String = mod_data.name if mod_data and mod_data.name else mod_id
 	$"%Title".text = "Configuring " + mod_name
 	
-	var config_key_regex := RegEx.new()
-	config_key_regex.compile("([a-z])([A-Z])")
 	
 	for key in mod_config.keys():
-		# Tackle Box currently only supports boolean config options
-		if typeof(mod_config[key]) != TYPE_BOOL: continue
+		if mod_config[key] is Array or mod_config[key] is Dictionary:
+			return
 		
 		var line := HBoxContainer.new()
 		line.rect_min_size.y = 40
 		
 		var label := Label.new()
 		label.set("size_flags_horizontal", 3)
-		label.text = config_key_regex.sub(key, "$1 $2", true) + ":"
+		label.text = key.capitalize().replace("_", " ") + ":"
 		label.set("custom_colors/font_color", Color(0.37, 0.24, 0.11))
+		
 		line.add_child(label)
 		
-		var option := OptionButton.new()
-		option.set("size_flags_horizontal", 3)
-		option.set("size_flags_stretch_ratio", 0.75)
+		if mod_config[key] is bool:
+			var option := OptionButton.new()
+			option.set("size_flags_horizontal", 3)
+			option.set("size_flags_stretch_ratio", 0.75)
+			
+			option.add_item("Enabled")
+			option.add_item("Disabled")
+			option.selected = 0 if mod_config[key] else 1
+			
+			_options[key] = option
 		
-		option.add_item("Enabled")
-		option.add_item("Disabled")
-		option.selected = 0 if mod_config[key] else 1
+			line.add_child(option)
+		else:
+			var text_input = LineEdit.new()
+			text_input.set("size_flags_horizontal", 3)
+			text_input.set("size_flags_stretch_ratio", 0.75)
+			
+			text_input.set("custom_colors/font_color", Color(1, 0.93, 0.84))
+			
+			text_input.text = str(mod_config[key])
+			
+			_options[key] = text_input
+			
+			line.add_child(text_input)
 		
-		_options[key] = option
-		
-		line.add_child(option)
 		$"%VBoxContainer".add_child(line)
 
 
@@ -49,7 +62,10 @@ func _update_config() -> int:
 	var mod_config: Dictionary = TackleBox.get_mod_config(_mod_id)
 	
 	for key in _options:
-		mod_config[key] = true if _options[key].selected == 0 else false
+		if _options[key] is OptionButton:
+			mod_config[key] = true if _options[key].selected == 0 else false
+		elif _options[key] is LineEdit:
+			mod_config[key] = _options[key].text if mod_config[key] is String else float(_options[key].text)
 	
 	return TackleBox.set_mod_config(_mod_id, mod_config)
 
